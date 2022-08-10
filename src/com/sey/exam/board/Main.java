552a750287a8384071149f337dc5952e8b016118
nyulnyul <nyul0223@naver.com>
2022-08-10 15:20:19 +0900

    입력받은 객체

--- src/com/sey/exam/board/Main.java
+++ src/com/sey/exam/board/Main.java
@@ -3,14 +3,14 @@ package com.sey.exam.board;
 import java.util.Scanner;
 
 
-class 게시물 {
-  void 게시(String usr, String article, int write) {
-    System.out.println("게시물 등록");
-    System.out.println("제목 : " + usr);
-    System.out.println("내용 : " + article);
-    System.out.printf("%d번 게시물이 등록되었습니다.\n", write);
-  }
+class Article{
+  int id;
+  String title;
+  String body;
 
+  void showingar(Article ar){
+    System.out.println("제목은 " + ar.title+ "\n내용은" + ar.body + "\n번호는" + ar.id);
+  }
 }
 
 
@@ -18,23 +18,35 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    int lastid = 0;
+
     System.out.println("==게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
     while (true) {
       System.out.printf("명령)");
       String cmd = sc.nextLine();
 
-      if (cmd.equals("/usr/article/write")) {
+      if (cmd.equals("see")) {
         System.out.println("==게시물 등록==");
-        System.out.println("제목 : ");
+        System.out.printf("제목 : \n");
         String title = sc.nextLine();
 
 
-        System.out.println("내용 : ");
+        System.out.printf("내용 : \n");
         String body = sc.nextLine();
+        int id = lastid + 1;
+        lastid++;
+          System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+          Article ar1 = new Article();
+          ar1.id = id;
+          ar1.body = body;
+          ar1.title = title;
+          ar1.showingar(ar1);
 
-        int num = 1;
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", num);
+        System.out.println("생성된 객체 : " +ar1);
+
+
+
       } else if (cmd.equals("exit")) {
         break;
       } else {
