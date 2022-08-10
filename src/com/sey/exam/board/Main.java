7b4f1fd694400893b30c4d304e4468a93cc8aadc
nyulnyul <nyul0223@naver.com>
2022-08-10 13:11:17 +0900

    명령어 입력

--- src/com/sey/exam/board/Main.java
+++ src/com/sey/exam/board/Main.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("==게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
+
+    System.out.printf("명령)");
+    String cmd = sc.nextLine();
+    System.out.printf("입력 받은 명령어 : %s\n" ,cmd );
+
     System.out.println("==프로그램 종료==");
   }
 }
