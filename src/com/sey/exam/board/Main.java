b19c8b1863ec88b1466abf00b3b95e3cc654192e
nyulnyul <nyul0223@naver.com>
2022-08-10 14:28:52 +0900

    새 게시물 입력받기

--- src/com/sey/exam/board/Main.java
+++ src/com/sey/exam/board/Main.java
@@ -2,17 +2,49 @@ package com.sey.exam.board;
 
 import java.util.Scanner;
 
+
+class 게시물 {
+  void 게시(String usr, String article, int write) {
+    System.out.println("게시물 등록");
+    System.out.println("제목 : " + usr);
+    System.out.println("내용 : " + article);
+    System.out.printf("%d번 게시물이 등록되었습니다.\n", write);
+  }
+
+}
+
+
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("==게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
+    while (true) {
+      System.out.printf("명령)");
+      String cmd = sc.nextLine();
 
-    System.out.printf("명령)");
-    String cmd = sc.nextLine();
-    System.out.printf("입력 받은 명령어 : %s\n" ,cmd );
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("==게시물 등록==");
+        System.out.println("제목 : ");
+        String title = sc.nextLine();
 
+
+        System.out.println("내용 : ");
+        String body = sc.nextLine();
+
+        int num = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", num);
+      } else if (cmd.equals("exit")) {
+        break;
+      } else {
+        System.out.printf("입력 받은 명령어 : %s\n", cmd);
+      }
+    }
     System.out.println("==프로그램 종료==");
+    sc.close();
+
+
   }
+
 }
