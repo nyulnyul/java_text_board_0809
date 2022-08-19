f6b3aad02622b95b3ee6bf57506074c811998c34
nyulnyul <nyul0223@naver.com>
2022-08-19 15:10:14 +0900

    검색

--- src/com/sey/exam/board/Main.java
+++ src/com/sey/exam/board/Main.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class Main {
   static void makeTestData(List<Article> articles) {
-    articles.add(new Article(1, "제목1", "내용1"));
-    articles.add(new Article(2, "제목2", "내용2"));
-    articles.add(new Article(3, "제목3", "내용3"));
+    for (int i = 0; i<100; i++){
+      int id = i+1;
+      articles.add(new Article(id, "제목"+id, "내용"+id));
+    }
+
   }
 
   public static void main(String[] args) {
@@ -34,12 +36,46 @@ public class Main {
         System.out.println("== 게시물 리스트 ==");
         System.out.println("-------------------");
         System.out.println("번호 / 제목 / 내용");
-        for (int i = articles.size() - 1; i >= 0; i--) {
-          Article article = articles.get(i);
-          System.out.printf("%d / %s / %s\n", article.id, article.title, article.body);
-        }
         System.out.println("-------------------");
-      } else if (rq.getUrlPath().equals("/usr/article/write")) {
+
+        //검색시작
+        List<Article> fileredArticles = articles;
+
+        if(params.containsKey("searchKeyword")) {
+          String searchKeyword = params.get("searchKeyword");
+
+          fileredArticles = new ArrayList<>();
+          for (Article article: articles) {
+            boolean matched = article.title.contains(searchKeyword) || article.body.contains(searchKeyword);
+
+            if (matched){
+              fileredArticles.add(article);
+            }
+
+          }
+
+        }
+        //검색끝
+
+        List<Article> sortedArticles =fileredArticles;
+
+        boolean orderByIdDesc = true;
+
+        if(params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")){
+          orderByIdDesc =false;
+
+        }
+        if(orderByIdDesc){
+          sortedArticles = Util.reverseList(sortedArticles);
+        }
+        for (Article article: sortedArticles) {
+          System.out.printf("%d/ %s/ %s\n",article.id , article.title , article.body);
+
+        }
+
+      }
+
+      else if (rq.getUrlPath().equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
@@ -59,7 +95,7 @@ public class Main {
         }
 
 
-        int id;
+        int id =0;
         try {
           id = Integer.parseInt(params.get("id"));
         }
@@ -74,6 +110,7 @@ public class Main {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
+
 
         System.out.println("== 게시물 상세내용 ==");
         System.out.printf("번호 : %d\n", article.id);
@@ -140,6 +177,7 @@ class Util {
     if (urlBits.length == 1) {
       return params;
     }
+
     String queryStr = urlBits[1];
     for (String bit : queryStr.split("&")) {
       String[] bits = bit.split("=", 2);
@@ -149,9 +187,18 @@ class Util {
       params.put(bits[0], bits[1]);
     }
     return params;
+
   }
 
   static String getUrlPathFromUrl(String url) {
     return url.split("\\?", 2)[0];
+  }
+  public static<T> List<T> reverseList(List<T> list) {
+    List<T> reverse = new ArrayList<>(list.size());
+
+    for ( int i = list.size() - 1; i >= 0; i-- ) {
+      reverse.add(list.get(i));
+    }
+    return reverse;
   }
 }
